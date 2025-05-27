7e803191cebedafe1cd59580958c00f5981d459c
lchqq <31716281+lchqq@users.noreply.github.com>
2025-05-27 10:04:12 +0800

    Update Application.java
    add sdf
    
    Signed-off-by: lchqq <31716281+lchqq@users.noreply.github.com>

--- agents/reflection/src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
+++ agents/reflection/src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import java.text.SimpleDateFormat;
 
 import java.util.Scanner;
 
@@ -15,12 +16,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    public static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    
     @Bean
     CommandLineRunner cli(ReflectionAgent reflectionAgent) {
         return args -> {
             var scanner = new Scanner(System.in);
             System.out.println("\nLet's chat!");
-
+            System.out.println("Date time:" + sdf.format(new Date()));
             // Generate a Java implementation of the Merge Sort algorithm
             while (true) {
                 System.out.print("\nUSER: ");
